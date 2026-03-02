d97ce639d9e0b75f08caba69d4b144ab52bbf6db
Paris <117606280+prs2772@users.noreply.github.com>
2026-03-02 02:12:58 -0600

    AVTS-001 Add Core

---
+++ AutoFleet.Core/Entities/Vehicle.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace AutoFleet.Core.Entities;
+
+public class Vehicle
+{
+    public int Id { get; set; }
+    public string Vin { get; set; } = string.Empty; // Vehicle Identification Number
+    public string Brand { get; set; } = string.Empty;
+    public string Model { get; set; } = string.Empty;
+    [Range(1900, 2059)] // Business rule validation: Year must be reasonable, avoids mistyping errors.
+    public int Year { get; set; }
+    public decimal Price { get; set; }
+    public bool IsSold { get; set; }
+
+    // 20260221 + PRS: Added new fields to comply with requirements
+    public int PassengerCapacity { get; set; }
+    public VehicleStatus Status { get; set; } = VehicleStatus.Available;
+    public decimal KmPerLiter { get; set; }
+}
---
+++ AutoFleet.Core/Interfaces/IVehicleRepository.cs
@@ -0,0 +1,33 @@
+using AutoFleet.Core.Entities;
+using AutoFleet.Core.Models;
+using AutoFleet.Core.Enums;
+
+namespace AutoFleet.Core.Interfaces;
+
+/// <summary>
+/// Manages our contract for Vehicle operations, both for single vehicles and fleet summaries.
+/// </summary>
+public interface IVehicleRepository
+{
+    /// <summary>
+    /// Identifies the source as MSSQL or Mongo
+    /// </summary>
+    RepositorySource Source { get; }
+
+    #region SingleVehicle Management
+    Task<IEnumerable<Vehicle>> GetAllAsync();
+    Task<Vehicle?> GetByVinAsync(string vin);
+    Task AddAsync(Vehicle vehicle);
+
+    #endregion
+
+    #region Fleet Management
+    // 20260221 + PRS: Added contact to get summary of available fleet
+    /// <summary>
+    /// Obtains a list of each Vehicle grouped by Name and their availability at the moment
+    /// </summary>
+    /// <returns>List of different vehicles availables</returns>
+    Task<List<InventoryItem>> GetAvailableFleetSummaryAsync();
+
+    #endregion
+}
